e92141ab9b90e786cfdd06c7bce9ba2a565f8f16
sunhahaha333 <sunshijie327@163.com>
2026-09-22 23:00:33 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-const V='cat-time-v2';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.hostname.includes('gtimg.cn')){e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(V).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)));return}if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const cp=n.clone();caches.open(V).then(c=>c.put(e.request,cp));return n}).catch(()=>caches.match('./index.html'))))});
-
